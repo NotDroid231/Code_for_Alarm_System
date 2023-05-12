1a23ba2a9cdd78ed1055ce9acbbe01c52d13c3e9
NotDroid231 <98360187+NotDroid231@users.noreply.github.com>
2023-05-12 11:45:52 -0400

    painful noise and other stuff

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
+let robbery = 0
+music.setVolume(255)
 basic.showString("ACTIVE")
 loops.everyInterval(1, function () {
-	
+    if (robbery >= 500) {
+        music.playMelody("C5 C C C5 C5 C C C5 ", 9006)
+    }
+})
+loops.everyInterval(1, function () {
+    if (robbery >= 500) {
+        music.playMelody("A E E A A E E A ", 60)
+    }
+})
+loops.everyInterval(1, function () {
+    if (robbery >= 500) {
+        music.playMelody("G F F G G F F G ", 1000)
+    }
 })
 basic.forever(function () {
-	
+    robbery = pins.analogReadPin(AnalogPin.P2)
 })

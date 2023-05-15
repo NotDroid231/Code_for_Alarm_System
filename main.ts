2910f54e587dd9640fe43357a48097d0e63b23eb
droidbetter <98360187+NotDroid231@users.noreply.github.com>
2023-05-15 11:30:53 -0400

    it is less painful and actualy senses stuff

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ music.setVolume(255)
 basic.showString("ACTIVE")
 loops.everyInterval(1, function () {
     if (robbery >= 500) {
-        music.playMelody("C5 C C C5 C5 C C C5 ", 600)
-    }
-})
-loops.everyInterval(1, function () {
-    if (robbery >= 500) {
         music.playMelody("G F F G G F F G ", 700)
+    } else if (robbery >= 200) {
+        music.playMelody("G G G G G G G G ", 120)
+        music.playMelody("F F F F F F F F ", 120)
+    } else {
+    	
     }
 })
 basic.forever(function () {

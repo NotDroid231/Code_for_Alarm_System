528755917fb70eff30e19670ed9f0aa458ac4987
droidbetter <98360187+NotDroid231@users.noreply.github.com>
2023-05-16 11:14:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ loops.everyInterval(1, function () {
     if (robbery >= 500) {
         music.playMelody("G F F G G F F G ", 700)
     } else if (robbery >= 200) {
-        music.playMelody("G G G G G G G G ", 120)
-        music.playMelody("F F F F F F F F ", 120)
+        music.playMelody("G G G G G G G G ", 300)
+        music.playMelody("F F F F F F F F ", 300)
     } else {
     	
     }

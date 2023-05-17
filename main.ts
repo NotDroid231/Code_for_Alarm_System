23c950ff82586b645e88d191c51004e6ad8b0a77
droidbetter <98360187+NotDroid231@users.noreply.github.com>
2023-05-17 11:20:11 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("RESET")
+    control.reset()
+})
 let robbery = 0
 music.setVolume(255)
 basic.showString("ACTIVE")
-music.playMelody("E E F F G G A A ", 120)
-music.playMelody("A A G G F F E E ", 120)
+music.playMelody("F F F F E E E E ", 120)
 loops.everyInterval(1, function () {
     if (robbery >= 800) {
         music.playMelody("G F F G G F F G ", 700)
-    } else if (robbery >= 700) {
+    } else if (robbery >= 400) {
         music.playMelody("G G G G G G G G ", 300)
-        music.playMelody("F F F F F F F F ", 300)
     } else {
     	
     }
